6b8bd6b11976bc93448ae196d6390a6756dcf033
SillOlga <Olga.Ivanova.sill@gmail.com>
2022-01-12 20:15:22 +0200

    Tablica izmenena

--- admin/modelAdmin/modelAdminNews.php
+++ admin/modelAdmin/modelAdminNews.php
@@ -3,9 +3,9 @@ class modelAdminNews{
 
 
     public static function getNewsList() {
-        $query = "SELECT news.*, category.name,users.username from news,
-        category, users WHERE news.category_id=category.id AND
-        news.user_id=users.id ORDER BY `news`.`id` DESC";
+        $query = "SELECT items.*, category.name,users.username from items,
+        category, users WHERE items.category_id=category.id AND
+        items.user_id=users.id ORDER BY `items`.`id` DESC";
         $db = new Database();
         $arr = $db->getAll($query);
         return $arr;
@@ -25,7 +25,7 @@ class modelAdminNews{
              //----------------------------images type blob
                   $image =addslashes (file_get_contents($_FILES['picture']['tmp_name']));
             //---------------------------------------------
-            $sql="INSERT INTO `news` (`id`, `title`, `text`, `picture`, `category_id`,
+            $sql="INSERT INTO `items` (`id`, `title`, `text`, `picture`, `category_id`,
             `user_id`) VALUES (NULL, '$title', '$text', '$image', '$idCategory', '1')";
                     $db = new Database();
                     $item = $db->executeRun($sql);
@@ -38,8 +38,8 @@ class modelAdminNews{
     }
     //------------------------------------news detail id
             public static function getNewsDetail($id) {
-        $query = "SELECT news.*, category.name,users.username from news, category, users
-        WHERE news.category_id=category.id AND news.user_id=users.id and news.id=".$id;
+        $query = "SELECT items.*, category.name,users.username from items, category, users
+        WHERE items.category_id=category.id AND items.user_id=users.id and items.id=".$id;
         $db = new Database();
         $arr = $db->getOne($query);
         return $arr;
@@ -65,12 +65,12 @@ class modelAdminNews{
             }
             //-------------------------------------------------------
             if($image==""){
-                $sql="UPDATE `news` SET `title` = '$title', `text` = '$text',
-                `category_id` = '$idCategory' WHERE `news`.`id` = ".$id;
+                $sql="UPDATE `items` SET `title` = '$title', `text` = '$text',
+                `category_id` = '$idCategory' WHERE `items`.`id` = ".$id;
             }
             else{
-                $sql="UPDATE `news` SET`title` = '$title', `text` = '$text', `picture`='
-                $image',`category_id` = '$idCategory' WHERE `news`.`id` = ".$id;
+                $sql="UPDATE `items` SET`title` = '$title', `text` = '$text', `picture`='
+                $image',`category_id` = '$idCategory' WHERE `items`.`id` = ".$id;
             }
 
                     $db = new Database();
@@ -88,7 +88,7 @@ class modelAdminNews{
     public static function getNewsDelete($id) {
         $test=false;
         if(isset($_POST['save']))    {
-            $sql="DELETE FROM `news` WHERE `news`.`id` = ".$id;
+            $sql="DELETE FROM `items` WHERE `items`.`id` = ".$id;
             $db = new Database();
                     $item = $db->executeRun($sql);
                 if($item==true){
